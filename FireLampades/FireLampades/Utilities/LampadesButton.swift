17b1e99c4058af3f4baf0bf7c811739b474b7a7c
Bartosz Kosarzycki <ext-bartosz.kosarzycki@here.com>
2017-01-20 14:43:02 +0100

    add custom button

---
+++ FireLampades/FireLampades/Utilities/LampadesButton.swift
@@ -0,0 +1,18 @@
+//
+//  LampadesButton.swift
+//  FireLampades
+//
+//  Created by Bartosz Kosarzycki on 1/20/17.
+//  Copyright © 2017 Bartosz Kosarzycki. All rights reserved.
+//
+
+import UIKit
+
+class LampadesButton: UIButton {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        backgroundColor = UIColor.red
+        
+    }
+}
