10f39b5dc4214436b5d330dc5f883634b308e6c6
Bartosz Kosarzycki <ext-bartosz.kosarzycki@here.com>
2017-01-20 14:33:11 +0100

    add control

--- FireLampades/FireLampades/View/LoginPanel/LoginPanelViewController.swift
+++ FireLampades/FireLampades/View/LoginPanel/LoginPanelViewController.swift
@@ -7,19 +7,26 @@
 //
 
 import UIKit
+import AnimatedTextInput
 
 private let loggedInPageSegue = "LoggedInPageSegue"
+
 
 class LoginPanelViewController: UIViewController, LoginPanelViewType {
     
     var loginPanelPresenter: LoginPanelPresenterType!
     
-    @IBOutlet var username: UITextField!
-    @IBOutlet var password: UITextField!
+    @IBOutlet var username: AnimatedTextInput!
+    @IBOutlet var password: AnimatedTextInput!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         loginPanelPresenter.view = self
+        
+        username.accessibilityLabel = "standard_text_input"
+        //username.type = .email
+        //username.style = CustomTextInputStyle()
+        username.placeHolderText = "aaaa"
     }
     
     @IBAction func submitButtonTapped(sender: UIButton) {
@@ -35,4 +42,22 @@ class LoginPanelViewController: UIViewController, LoginPanelViewType {
     func showUnauthorizedMessage() {
         //TODO implement
     }
+}
+
+struct CustomTextInputStyle: AnimatedTextInputStyle {
+    
+    let activeColor = UIColor.orange
+    let inactiveColor = UIColor.gray.withAlphaComponent(0.3)
+    let lineInactiveColor = UIColor.gray.withAlphaComponent(0.3)
+    let errorColor = UIColor.red
+    let textInputFont = UIFont.systemFont(ofSize: 14)
+    let textInputFontColor = UIColor.black
+    let placeholderMinFontSize: CGFloat = 9
+    let counterLabelFont: UIFont? = UIFont.systemFont(ofSize: 9)
+    let leftMargin: CGFloat = 25
+    let topMargin: CGFloat = 20
+    let rightMargin: CGFloat = 0
+    let bottomMargin: CGFloat = 10
+    let yHintPositionOffset: CGFloat = 7
+    let yPlaceholderPositionOffset: CGFloat = 0
 }
