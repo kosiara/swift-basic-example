af2568c4327773b3d96b40faeca8079a8b4f7010
Bartosz Kosarzycki <ext-bartosz.kosarzycki@here.com>
2017-01-25 15:13:17 +0100

    currentsize

--- FireLampades/FireLampades/Utilities/UIImage+Tint.swift
+++ FireLampades/FireLampades/Utilities/UIImage+Tint.swift
@@ -10,17 +10,17 @@ import UIKit
 
 extension UIImage {
     func tintImage(color: UIColor) -> UIImage? {
-        let size = self.size
+        let currentSize = size
         
-        UIGraphicsBeginImageContextWithOptions(size, false, self.scale)
-        guard let context = UIGraphicsGetCurrentContext() else { return UIImage() }
+        UIGraphicsBeginImageContextWithOptions(currentSize, false, scale)
+        guard let context = UIGraphicsGetCurrentContext() else { return nil }
         self.draw(at: CGPoint(x: 0, y: 0), blendMode: CGBlendMode.normal, alpha: 1.0)
         
         context.setFillColor(color.cgColor)
         context.setBlendMode(CGBlendMode.sourceIn)
         context.setAlpha(1.0)
         
-        let rect = CGRect(x: 0, y: 0, width: self.size.width, height: self.size.height)
+        let rect = CGRect(x: 0, y: 0, width: size.width, height: size.height)
         UIGraphicsGetCurrentContext()?.fill(rect)
         let tintedImage = UIGraphicsGetImageFromCurrentImageContext()
         UIGraphicsEndImageContext()
